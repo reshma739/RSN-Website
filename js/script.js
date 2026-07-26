ca32e0a2918df34559899fc1a3030f30f3dfa412
Reshma <srreshma198@gmail.com>
2026-07-26 09:48:10 +0530

    java

--- js/script.js
+++ js/script.js
@@ -89,3 +89,39 @@ searchInput.addEventListener("keyup", function () {
     });
 
 });
+
+
+
+<script>
+
+const searchInput = document.getElementById("stationerySearch");
+
+const products = document.querySelectorAll(".product-card");
+
+
+searchInput.addEventListener("keyup", function(){
+
+    let searchText = searchInput.value.toLowerCase();
+
+
+    products.forEach(function(product){
+
+        let productName = product.innerText.toLowerCase();
+
+
+        if(productName.includes(searchText)){
+
+            product.style.display = "";
+
+        }
+        else{
+
+            product.style.display = "none";
+
+        }
+
+    });
+
+});
+
+</script>
