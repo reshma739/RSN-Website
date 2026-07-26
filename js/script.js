e17379f1ee809b5658e571924c6732d8c8673ae1
Reshma <srreshma198@gmail.com>
2026-07-26 07:33:03 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,91 @@
+//===============================
+// Scroll Animation
+//===============================
+
+const animatedSections = document.querySelectorAll(".animate");
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach((entry)=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:0.2
+});
+
+animatedSections.forEach((section)=>{
+
+    observer.observe(section);
+
+});
+
+const timelineItems = document.querySelectorAll(".timeline-item");
+
+timelineItems.forEach((item)=>{
+
+    observer.observe(item);
+
+});
+
+
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faq => {
+
+    const button = faq.querySelector(".faq-question");
+
+    button.addEventListener("click", () => {
+
+        faqs.forEach(item => {
+
+            if(item !== faq){
+
+                item.classList.remove("active");
+
+            }
+
+        });
+
+        faq.classList.toggle("active");
+
+    });
+
+});
+
+ //==============================
+// BOOK SEARCH
+//==============================
+
+const searchInput = document.getElementById("searchInput");
+
+const cards = document.querySelectorAll(".book-card");
+
+searchInput.addEventListener("keyup", function () {
+
+    let searchValue = searchInput.value.toLowerCase();
+
+    cards.forEach(function(card){
+
+        let title = card.querySelector("h3").textContent.toLowerCase();
+
+        if(title.includes(searchValue)){
+
+            card.style.display = "block";
+
+        }else{
+
+            card.style.display = "none";
+
+        }
+
+    });
+
+});
